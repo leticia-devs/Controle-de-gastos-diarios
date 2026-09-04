ae3a68d0d09a14ec703992153ad67629bb9a8a9d
Leticia K Ferreira <dev_leticia@hotmail.com>
2026-09-03 22:30:20 -0300

    Update app.py
    Versão 1.0 para testes.

--- app.py
+++ app.py
@@ -10,3 +10,21 @@ conexao = mysql.connector.connect(
 cursor = conexao.cursor()
 
 print("--- Cadastrar Nova Despesa ---")
+valor = float(input("Digite o valor gasto (ex: 45.90): "))
+categoria = input("Digite a categoria (ex: Alimentação, Transporte): ")
+descricao = input("Digite uma descrição (100 caracteres): ")
+data = input("Digite a data (AAAA-MM-DD): ")
+
+comando = """
+    INSERT INTO despesas (valor, categoria, descricao, data) 
+    VALUES (%s, %s, %s, %s)
+"""
+
+valores = (valor, categoria, descricao, data)
+cursor.execute(comando, valores)
+conexao.commit()
+
+print("Despesa cadastrada com sucesso!")
+
+cursor.close()
+conexao.close()
